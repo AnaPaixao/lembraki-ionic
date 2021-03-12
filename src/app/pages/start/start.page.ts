5e4fd9edfb909fa710847ef4be25de2a6d7ce461
Tecnico em informatica 2018.1 <42121742018.1@senacrjedu.local>
2021-03-11 21:59:39 -0300

    visualização do card iniciada, problemas com alinhamento

--- src/app/pages/start/start.page.ts
+++ src/app/pages/start/start.page.ts
@@ -2,7 +2,7 @@ import { DecksService } from './../../services/decks.service';
 import { Observable } from 'rxjs';
 import { ActivatedRoute } from '@angular/router';
 import { CardsService } from './../../services/cards.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { AuthService } from './../../services/auth.service';
 import { Card } from 'src/app/classes/card';
 
@@ -23,6 +23,10 @@ export class StartPage implements OnInit {
   deckLength: number;
 
   index: number = 0;
+
+
+  @ViewChild('cardBackContent') cardBackContent: ElementRef;
+  @ViewChild('backInner') cardBackInner: ElementRef;
 
   constructor(
     private auth: AuthService,
@@ -61,6 +65,25 @@ export class StartPage implements OnInit {
     });
   }
 
+  ngDoCheck(){
+
+  }
+
+  ngAfterViewChecked(){
+    // if(this.cardBackContent){
+    //   console.log(this.cardBackContent.nativeElement);
+    // }
+
+    if(this.cardBackInner && this.cardBackContent){
+      console.log(this.cardBackInner.nativeElement.textContent.length);
+
+      if(this.cardBackInner.nativeElement.textContent.length < 200){
+        this.cardBackContent.nativeElement.classList.add("flex-center")
+      } else {
+        this.cardBackContent.nativeElement.classList.remove("flex-center")
+      }
+    }
+  }
 
   Remember() {
     this.incrementIndex();
